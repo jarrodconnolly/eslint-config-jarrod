e06454472a0272de182813934005eeebb6b622e0
Jarrod Connolly <jarrod@nestedquotes.ca>
2021-03-10 00:29:00 -0800

    feat: inital release

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'jest': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': "script",
+  },
+  'rules': {
+    "semi": "error"
+  },
+};
